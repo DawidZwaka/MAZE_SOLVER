dbf265bd1ddccb7831662f2bfb48f27e69fbfc60
unknown <zwakadawid@gmail.com>
2021-11-21 19:46:09 +0100

    solver done!

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const directions = {
+	TOP: 0,
+	LEFT: 1,
+	BOTTOM: 2,
+	RIGHT: 3,
+};
+
+function timeout(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 class MazeRenderer {
 	static wallColor = "#111111";
 	static pathColor = "#f1f1f1";
@@ -41,13 +52,39 @@ class MazeRenderer {
 class SolverRenderer {
 	static solverColor = "#ff0000";
 
-	static drawSolver(renderer, x, y, direction = "top") {
-		const { pixelSize, renderOffset } = renderer;
+	static drawSolver(renderer, x, y) {
+		const { pixelSize } = renderer;
 
-		renderer.ctx.moveTo(x + pixelSize / 2, y);
+		renderer.ctx.beginPath();
 
-		renderer.ctx.lineTo(x + pixelSize, y + pixelSize);
-		renderer.ctx.lineTo(x, y + pixelSize);
+		console.log(Solver.direction);
+
+		switch (Solver.direction) {
+			case directions.TOP: {
+				renderer.ctx.moveTo(x + pixelSize / 2, y);
+				renderer.ctx.lineTo(x + pixelSize, y + pixelSize);
+				renderer.ctx.lineTo(x, y + pixelSize);
+				break;
+			}
+			case directions.LEFT: {
+				renderer.ctx.moveTo(x + pixelSize, y);
+				renderer.ctx.lineTo(x, y + pixelSize / 2);
+				renderer.ctx.lineTo(x + pixelSize, y + pixelSize);
+				break;
+			}
+			case directions.BOTTOM: {
+				renderer.ctx.moveTo(x, y);
+				renderer.ctx.lineTo(x + pixelSize / 2, y + pixelSize);
+				renderer.ctx.lineTo(x + pixelSize, y);
+				break;
+			}
+			case directions.RIGHT: {
+				renderer.ctx.moveTo(x, y);
+				renderer.ctx.lineTo(x + pixelSize, y + pixelSize / 2);
+				renderer.ctx.lineTo(x, y + pixelSize);
+				break;
+			}
+		}
 
 		renderer.ctx.fill();
 	}
@@ -72,6 +109,86 @@ class SolverRenderer {
 	}
 }
 
+class Solver {
+	static direction = directions.TOP;
+
+	static getPosition() {
+		let position = -1;
+
+		Scene.sceneMap.forEach((row, rowIndex) => {
+			const inx = row.findIndex((col) => col === 2);
+
+			if (inx !== -1) position = [rowIndex, inx];
+		});
+
+		return position;
+	}
+
+	static go() {
+		const [x, y] = Solver.getPosition();
+
+		Scene.sceneMap[x][y] = 0;
+
+		switch (Solver.direction) {
+			case directions.TOP: {
+				Scene.sceneMap[x - 1][y] = 2;
+				break;
+			}
+			case directions.LEFT: {
+				Scene.sceneMap[x][y - 1] = 2;
+				break;
+			}
+			case directions.BOTTOM: {
+				Scene.sceneMap[x + 1][y] = 2;
+				break;
+			}
+			case directions.RIGHT: {
+				Scene.sceneMap[x][y + 1] = 2;
+				break;
+			}
+		}
+
+		console.log(Scene.sceneMap);
+	}
+
+	static rotateLeft90() {
+		let nextDirection = Solver.direction + 1;
+
+		if (nextDirection > 3) nextDirection = 4 % nextDirection;
+
+		Solver.direction = nextDirection;
+	}
+
+	static checkIfWall() {
+		const [x, y] = Solver.getPosition();
+		let mapPixel;
+		let res = false;
+
+		switch (Solver.direction) {
+			case directions.TOP: {
+				mapPixel = Scene.sceneMap[x - 1][y];
+				break;
+			}
+			case directions.LEFT: {
+				mapPixel = Scene.sceneMap[x][y - 1];
+				break;
+			}
+			case directions.BOTTOM: {
+				mapPixel = Scene.sceneMap[x + 1][y];
+				break;
+			}
+			case directions.RIGHT: {
+				mapPixel = Scene.sceneMap[x][y + 1];
+				break;
+			}
+		}
+
+		if (mapPixel === 1) res = true;
+
+		return res;
+	}
+}
+
 class Scene {
 	static sceneMap = [
 		[1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -87,14 +204,15 @@ class Scene {
 	];
 
 	static render(renderer) {
-		MazeRenderer.render(renderer, this.sceneMap);
-		SolverRenderer.render(renderer, this.sceneMap);
+		MazeRenderer.render(renderer, Scene.sceneMap);
+		SolverRenderer.render(renderer, Scene.sceneMap);
 	}
 }
 
 class CanvasRenderer {
 	static renderOffset = 0;
 	static pixelSize = 40;
+	static frameLength = 300;
 
 	static windowResizeHandler() {
 		CanvasRenderer.c.width = window.innerWidth;
@@ -108,19 +226,47 @@ class CanvasRenderer {
 		CanvasRenderer.windowResizeHandler();
 	}
 
-	static renderLoop(el) {
-		el.render();
+	static rerenderScene() {
+		CanvasRenderer.ctx.clearRect(
+			0,
+			0,
+			CanvasRenderer.c.width,
+			CanvasRenderer.c.height
+		);
 
-		requestAnimationFrame(CanvasRenderer.renderLoop);
+		Scene.render(CanvasRenderer);
+	}
+
+	static async renderLoop() {
+		CanvasRenderer.rerenderScene();
+		Solver.rotateLeft90();
+		await timeout(CanvasRenderer.frameLength);
+
+		if (Solver.checkIfWall()) {
+			CanvasRenderer.rerenderScene();
+			Solver.rotateLeft90();
+			await timeout(CanvasRenderer.frameLength);
+
+			CanvasRenderer.rerenderScene();
+			Solver.rotateLeft90();
+			await timeout(CanvasRenderer.frameLength);
+		} else {
+			CanvasRenderer.rerenderScene();
+			Solver.go();
+			await timeout(CanvasRenderer.frameLength);
+		}
+
+		CanvasRenderer.renderLoop();
 	}
 
 	static execute(el) {
 		CanvasRenderer.init();
 
-		//window.addEventListener("resize", CanvasRenderer.windowResizeHandler);
-
-		CanvasRenderer.renderLoop(el);
+		//CanvasRenderer.renderLoop(el);
+		//CanvasRenderer.renderLoop(el);
+		//setInterval(() => CanvasRenderer.renderLoop(el), 1000);
+		CanvasRenderer.renderLoop();
 	}
 }
 
-CanvasRenderer.execute(Scene);
+CanvasRenderer.execute();
